03802920adc5c48a7562f9778a7bece157d43611
jebrong <92987206+jebrong@users.noreply.github.com>
2022-11-16 20:47:26 +0800

    edit mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./index.css";
 
 function App() {
+  const getLocalStorage = () => {
+    let list = localStorage.getItem("list");
+    if (list) {
+      return JSON.parse(localStorage.list);
+    } else {
+      return [];
+    }
+  };
+
   const [item, setItem] = useState({ id: "", option: "" });
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
+  const [editMode, setEditMode] = useState(false);
+  getLocalStorage();
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
 
   const handleChange = (e) => {
     let id = new Date().getTime().toString();
+
     setItem((prev) => {
       return { ...prev, id: id, option: e.target.value };
     });
@@ -17,6 +33,8 @@ function App() {
     setList((prev) => {
       return [...prev, item];
     });
+    setItem({ id: "", option: "" });
+    setEditMode(false);
   };
 
   const handleDelete = (id) => {
@@ -26,7 +44,21 @@ function App() {
       })
     );
   };
-  const handleEdit = () => {};
+  const handleEdit = (ide) => {
+    setEditMode(true);
+
+    let x = list.find((e) => {
+      return e.id === ide;
+    });
+
+    setItem({ id: x.id, option: x.option });
+
+    setList(
+      list.filter((e) => {
+        return e.id !== ide;
+      })
+    );
+  };
   return (
     <div className="container">
       <h2>Grocery List</h2>
@@ -37,7 +69,8 @@ function App() {
           name="option"
           value={item.option}
         ></input>
-        <button className="button-27">ADD</button>
+
+        <button className="button-27">{editMode ? "EDIT" : "ADD"}</button>
       </form>
       <div className="container__list">
         {list.map((e) => {
@@ -45,13 +78,15 @@ function App() {
             <div className="container__list_item" key={e.id}>
               <h4 className="text">{e.option}</h4>
               <button
+                className="container__list_item_btn"
                 onClick={() => {
-                  handleEdit(e);
+                  handleEdit(e.id);
                 }}
               >
                 EDIT
               </button>
               <button
+                className="container__list_item_btn"
                 onClick={() => {
                   handleDelete(e.id);
                 }}
